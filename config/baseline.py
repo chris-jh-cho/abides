d61e7a178939a3b143f3beb037b09c8bdf78f05b
Chris Cho <chris.j.cho@gmail.com>
2020-10-12 21:10:36 +0100

    added new baseline

--- config/baseline.py
+++ config/baseline.py
@@ -188,7 +188,7 @@ zi = [ (75, 0, 250, 1), (75, 0, 500, 1), (70, 0, 1000, 0.8), (70, 0, 1000, 1), (
 # time step is a nanosecond, and we want the agents to arrive more on the order of
 # minutes.
 for i,x in enumerate(zi):
-  strat_name = "Type {} {} {} {}".format(i+1, x[1], x[2], x[3])
+  strat_name = "Type {} [{} <= R <= {}, eta={}]".format(i+1, x[1], x[2], x[3])
   agents.extend([ ZeroIntelligenceAgent(j, "ZI Agent {} {}".format(j, strat_name), "ZeroIntelligenceAgent {}".format(strat_name), random_state = np.random.RandomState(seed=np.random.randint(low=0,high=2**32, dtype='uint64')),log_orders=log_orders, symbol=symbol, starting_cash=starting_cash, sigma_n=sigma_n, r_bar=s['r_bar'], kappa=s['agent_kappa'], sigma_s=s['fund_vol'], q_max=10, sigma_pv=5e6, R_min=x[1], R_max=x[2], eta=x[3], lambda_a=1e-12) for j in range(agent_count,agent_count+x[0]) ])
   agent_types.extend([ "ZeroIntelligenceAgent {}".format(strat_name) for j in range(x[0]) ])
   agent_count += x[0]
