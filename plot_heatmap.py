ae59244d4c4990cb8b858a27bc5d96591d3e4f64
Chris Cho <chris.j.cho@gmail.com>
2020-11-05 14:59:13 +0000

    Small edit to the plot heatmap function

--- plot_heatmap.py
+++ plot_heatmap.py
@@ -6,13 +6,15 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-log = pd.read_pickle("ORDERBOOK_JPM_FREQ_T.bz2")
+log = pd.read_pickle("ORDERBOOK_IBM_FREQ_T.bz2")
 
 df2 = log.reset_index().pivot(columns='quote',index='time',values='Volume')
 
 sns.heatmap(df2, cmap = "RdBu", vmin = -300, vmax = 300)
 
 plt.show()
+
+
 
 
 midprice = np.zeros(log.shape[0])
