d91fa62306db6accd7c1794ecc2ad5dd1449d008
Chris Cho <chris.j.cho@gmail.com>
2020-11-23 14:43:45 +0000

    Cleaned up the heatmap function

--- plot_heatmap.py
+++ plot_heatmap.py
@@ -13,17 +13,3 @@ df2 = log.reset_index().pivot(columns='quote',index='time',values='Volume')
 sns.heatmap(df2, cmap = "RdBu", vmin = -300, vmax = 300)
 
 plt.show()
-
-
-
-
-midprice = np.zeros(log.shape[0])
-for i in range(log.shape[0]):
-    print(i)
-    current_book = log.iloc[i,:]
-    buy_orders = current_book[current_book < 0]
-    sell_orders = current_book[current_book > 0]
-    if (len(buy_orders) == 0 or len(sell_orders) == 0):
-        midprice[i] = np.nan
-    else: 
-        midprice[i] = (buy_orders.index[-1] + sell_orders.index[0])/2
